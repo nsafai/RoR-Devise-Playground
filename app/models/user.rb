3dcee405df231db4b61d9a5388e2cd72f64b7bce
Nicolai Safai <nicolaisafai@gmail.com>
2018-11-13 15:22:58 -0800

    mvp complete:

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validates :is_signed_in, inclusion: [true, false]
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  after_create :notify_pusher
+  def notify_pusher
+    Pusher.trigger('activity', 'login', self.as_json)
+  end
+
+  def as_json(options={})
+    super(
+      only: [:id, :email, :username]
+    )
+  end
+end
